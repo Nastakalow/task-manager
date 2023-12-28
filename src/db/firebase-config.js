3de5151c77d6014dccf3cac0d0610106a462afa7
Ramazan Nastakalov <nastakalow@gmail.com>
2023-12-29 01:28:48 +0400

    fix: change firebase-config

--- src/db/firebase-config.js
+++ src/db/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB0ZvBK6D6zojPgXXRecdq0TUEDmc_kpoM",
-  authDomain: "task-manager-42710.firebaseapp.com",
-  projectId: "task-manager-42710",
-  storageBucket: "task-manager-42710.appspot.com",
-  messagingSenderId: "924414194681",
-  appId: "1:924414194681:web:a5436aca2e71a10c474328",
-  measurementId: "G-BJ1CV1SFJ2",
+  apiKey: "AIzaSyBOObugY71dbiCK_BiH95xLBTadV4Z3Tk0",
+  authDomain: "task-manager-30813.firebaseapp.com",
+  projectId: "task-manager-30813",
+  storageBucket: "task-manager-30813.appspot.com",
+  messagingSenderId: "308484578601",
+  appId: "1:308484578601:web:02cf23171866073c772b38",
+  measurementId: "G-XC0MW56ESM",
 };
 
 export const app = initializeApp(firebaseConfig);
